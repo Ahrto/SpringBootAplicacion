d5edd4f0c44c788fe5aebdc5213fb3d965b50497
Alberto Hernandez <albertohernanrodri@gmail.com>
2020-12-16 17:48:25 +0100

    First Commit

---
+++ src/main/java/com/alberto/aplicacion/SpringBootAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.alberto.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAplicacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alberto/aplicacion/SpringBootAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
